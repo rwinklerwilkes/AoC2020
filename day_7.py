37d0404e98fe93323976c71f0a3ee8753ef3eb92
rwinklerwilkes <rich.w.sas@gmail.com>
2021-11-20 08:39:22 -0500

    Day Seven, Part Two

--- day_7.py
+++ day_7.py
@@ -2,15 +2,15 @@ from aocd import get_data
 import re
 
 input_data = get_data(year=2020, day=7)
-# test_data = """light red bags contain 1 bright white bag, 2 muted yellow bags.
-# dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-# bright white bags contain 1 shiny gold bag.
-# muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-# shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-# dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-# vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-# faded blue bags contain no other bags.
-# dotted black bags contain no other bags."""
+test_data = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."""
 
 
 def process_entry(raw):
@@ -47,6 +47,12 @@ class Bag:
 
     def can_contain(self, other, number):
         self.contains[other] = number
+
+    def eventually_contains(self):
+        total = 0
+        for bag, number in self.contains.items():
+            total += number * (bag.eventually_contains()+1)
+        return total
 
 
 def process_data(input_data):
@@ -96,8 +102,12 @@ def find_answer(input_data):
 
 
 def find_answer_part_two(input_data):
-    pass
+    contains, contained_by = process_data(input_data)
+    answer = contains['shiny gold'].eventually_contains()
+    print(answer)
+    return answer
 
 
 # contains, contained_by = process_data(test_data)
 answer = find_answer(input_data)
+answer = find_answer_part_two(input_data)
