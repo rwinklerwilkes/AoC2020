92aa411395d491151a920e5ef93522e5205a27d3
rwinklerwilkes <rich.w.sas@gmail.com>
2021-11-25 12:25:01 -0500

    Day Sixteen, Part One

--- day_16.py
+++ day_16.py
@@ -16,11 +16,51 @@ nearby tickets:
 38,6,12"""
 
 def parse_rule(rule):
-    rule_regex = r'^([a-z]{1,}): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)$'
+    rule_regex = r'^([a-z\s]{1,}): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)$'
     match_obj = re.match(rule_regex, rule)
     rule_name = match_obj.group(1)
     rule_limits = [[int(match_obj.group(2)), int(match_obj.group(3))], [int(match_obj.group(4)), int(match_obj.group(5))]]
     return rule_name, rule_limits
 
-def parse_ticket(ticket):
+def parse_all_rules(rules):
+    rule_demarcations = {}
+    for rule_name, rule_limits in rules:
+        for lower_limit, upper_limit in rule_limits:
+            for i in range(lower_limit, upper_limit + 1):
+                rules_at_this_location = rule_demarcations.get(i,[])
+                rules_at_this_location.append(rule_name)
+                rule_demarcations[i] = rules_at_this_location
+    return rule_demarcations
+
+def parse_input_data(input_data):
+    sections = input_data.split('\n\n')
+    rules = sections[0].split('\n')
+    your_ticket = parse_ticket(sections[1].split('\n')[1])
+    other_tickets = [parse_ticket(ticket) for ticket in sections[2].split('\n')[1:]]
+    all_rules = [parse_rule(rule) for rule in rules]
+    all_rules = parse_all_rules(all_rules)
+    return all_rules, your_ticket, other_tickets
+
+def check_ticket(ticket, all_rules):
+    invalid_values = []
+    for value in ticket:
+        if len(all_rules.get(value,[])) == 0:
+            invalid_values.append(value)
+    is_valid = len(invalid_values) == 0
+    return is_valid, invalid_values
+
+def find_answer_part_one(input_data):
+    all_rules, your_ticket, other_tickets = parse_input_data(input_data)
+    invalid_rules = []
+    for ticket in other_tickets:
+        is_valid, invalid_rules_this_ticket = check_ticket(ticket, all_rules)
+        invalid_rules += invalid_rules_this_ticket
+    print(sum(invalid_rules))
+
+def find_answer_part_two():
     pass
+
+def parse_ticket(ticket):
+    return [int(i) for i in ticket.split(',')]
+
+find_answer_part_one(input_data)
